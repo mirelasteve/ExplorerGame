551cfdae3e109502c767555c1563f9a0c7807041
mirelasteve <mirelasteve@abv.bg>
2020-06-19 14:32:47 +0300

    authReducer changes

--- src/Component/redux/reducers/authReducer.js
+++ src/Component/redux/reducers/authReducer.js
@@ -1,10 +1,10 @@
 
   export default function auth(state={},action){
     switch(action.type){
-        case 'GOOGLE_SIGN_UP': return action.user
-        case 'FACEBOOK_SIGN_UP': return action.user
-        case 'SIGN_UP': return action.user
-        case 'AUTH_USER': return action.user
+        case 'GOOGLE_SIGN_UP': return {...state,...action.user}
+        case 'FACEBOOK_SIGN_UP': return {...state,...action.user}
+        case 'SIGN_UP': return {...state,...action.user}
+        case 'AUTH_USER': return {...state,...action.user}
         default: return state
     }
 }
